1941453d91f96d9d1fee9a5b40713080d8a48a5e
Peter-PM <tangompp@gmail.com>
2021-03-15 00:54:48 +0300

    validations

--- js/form.js
+++ js/form.js
@@ -2,13 +2,85 @@ import { resetMainMarker } from './map.js';
 import { postData } from './api.js';
 import { onPopupErrorShow, onPopupSuccessShow } from './popup.js';
 
+
+const MIN_NAME_LENGTH = 30;
+const MAX_NAME_LENGTH = 100;
+
 const mainForm = document.querySelector('.ad-form');
 
-const typeHousing = document.querySelector('#type');
-const priceHousing = document.querySelector('#price');
+const typeHousing = mainForm.querySelector('#type');
+const priceHousing = mainForm.querySelector('#price');
 
-const timeIn = document.querySelector('#timein');
-const timeOut = document.querySelector('#timeout');
+const timeIn = mainForm.querySelector('#timein');
+const timeOut = mainForm.querySelector('#timeout');
+
+const roomNumber = mainForm.querySelector('#room_number');
+const guestNumber = mainForm.querySelector('#capacity');
+const clarification = mainForm.querySelector('.clarification');
+let numberKey = roomNumber.value;
+
+const guestNumberKeysMore = {
+  1 : {
+    en : [1],
+    dis : [2,3,0],
+  },
+  2 : {
+    en : [1,2],
+    dis : [3,0],
+  },
+  3 : {
+    en : [1,2,3],
+    dis : [0],
+  },
+  100 : {
+    en : [0],
+    dis : [1,2,3],
+  },
+}
+
+const setAttributeGuestOption = (num) => {
+  guestNumber.querySelector(`option[value="${num}"]`).disabled = true;
+};
+const removeAttributeGuestOption = (number) => {
+  guestNumber.querySelector(`option[value="${number}"]`).disabled = false;
+};
+
+const disabledGuestOption = () => {
+  clarification.style.display = 'none';
+  guestNumberKeysMore[numberKey].dis.forEach((number) => {
+    setAttributeGuestOption(number);
+  });
+};
+disabledGuestOption();
+
+roomNumber.addEventListener('change', () => {
+  numberKey = roomNumber.value;
+  guestNumberKeysMore[numberKey].en.forEach((number) => {
+    guestNumber.value = `${number}`;
+    removeAttributeGuestOption(number);
+  });
+  guestNumberKeysMore[numberKey].dis.forEach((number) => {
+    setAttributeGuestOption(number);
+  });
+});
+
+
+const userNameInput = document.querySelector('#title');
+
+userNameInput.addEventListener('input', () => {
+  const valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Ещё ' + (MIN_NAME_LENGTH - valueLength) +' симв.');
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_NAME_LENGTH) +' симв.');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+
+  userNameInput.reportValidity();
+});
+
 
 const housingPrice = {
   bungalow: 0,
